265bb3b2e8bfb2bee43a5ed9addf99910398a21d
Max Scher and Nicole Freed <nicole@>
2017-07-03 13:55:56 -0700

    delete subsidiary methods and clean up

--- src/PingPongGenerator.php
+++ src/PingPongGenerator.php
@@ -9,67 +9,7 @@
 <?php
     class PingPongGenerator
     {
-        // rejectNoInput
-        function rejectNoInput($num)
-        {
-            if (empty($num)) {
-                return "Please enter a number.";
-            }
-        }
-
-        // // reject whitespace
-        function rejectWhitespace($num)
-        {
-            if (strpbrk($num," ")) {
-                return "Spaces are not allowed.";
-            }
-        }
-
-        // // reject non-numeric input
-        function rejectNonNum($num)
-        {
-            if (!(is_numeric($num))) {
-                return "Please enter a number.";
-            }
-        }
-
-        // // basic count method
         function count($num)
-        {
-            $num_arr = [];
-            for ($i = 1; $i <= $num; $i++) {
-                array_push($num_arr, $i);
-            }
-            var_dump($num_arr);
-            return $num_arr;
-        }
-
-        // // return ping
-        function ping($num)
-        {
-            if ($num % 3 == 0) {
-                return "ping";
-            }
-        }
-
-        // // return pong
-        function pong($num)
-        {
-            if ($num % 5 == 0) {
-                return "pong";
-            }
-        }
-
-        // return ping-pong
-        function pingPong($num)
-        {
-            if ($num % 15 == 0) {
-                return "ping-pong";
-            }
-        }
-
-        // combining all methods
-        function combineCount($num)
         {
             $num_arr = [];
             if (empty($num)) {
--- tests/PingPongGeneratorTest.php
+++ tests/PingPongGeneratorTest.php
@@ -4,112 +4,21 @@
     class PingPongGeneratorTest extends PHPUnit_Framework_TestCase
     {
 
-        function test_reject_noInput()
-        {
-            //Arrange
-            $test_PingPongGenerator = new PingPongGenerator;
-            $input = "";
-
-            //Act
-            $result = $test_PingPongGenerator->rejectNoInput($input);
-
-            //Assert
-            $this->assertEquals("Please enter a number.", $result);
-        }
-
-        function test_reject_whitespace()
-        {
-            //Arrange
-            $test_PingPongGenerator = new PingPongGenerator;
-            $input = " ";
-
-            //Act
-            $result = $test_PingPongGenerator->rejectWhitespace($input);
-
-            //Assert
-            $this->assertEquals("Spaces are not allowed.", $result);
-        }
-
-        function test_reject_nonNum()
-        {
-            //Arrange
-            $test_PingPongGenerator = new PingPongGenerator;
-            $input = "r@nd0m_nVmb3r$!#";
-
-            //Act
-            $result = $test_PingPongGenerator->rejectNonNum($input);
-
-            //Assert
-            $this->assertEquals("Please enter a number.", $result);
-        }
-
         function test_count()
-        {
-            //Arrange
-            $test_PingPongGenerator = new PingPongGenerator;
-            $input = "5";
-
-            //Act
-            $result = $test_PingPongGenerator->count($input);
-
-            //Assert
-            $this->assertEquals([1, 2, 3, 4, 5], $result);
-        }
-
-        function test_ping()
-        {
-            //Arrange
-            $test_PingPongGenerator = new PingPongGenerator;
-            $input = "3";
-
-            //Act
-            $result = $test_PingPongGenerator->ping($input);
-
-            //Assert
-            $this->assertEquals("ping", $result);
-        }
-
-        function test_pong()
-        {
-            //Arrange
-            $test_PingPongGenerator = new PingPongGenerator;
-            $input = "5";
-
-            //Act
-            $result = $test_PingPongGenerator->pong($input);
-
-            //Assert
-            $this->assertEquals("pong", $result);
-        }
-
-        function test_pingPong()
-        {
-            //Arrange
-            $test_PingPongGenerator = new PingPongGenerator;
-            $input = "15";
-
-            //Act
-            $result = $test_PingPongGenerator->pingPong($input);
-
-            //Assert
-            $this->assertEquals("ping-pong", $result);
-        }
-
-        function test_combineCount()
         {
             //Arrange
             $test_PingPongGenerator = new PingPongGenerator;
             // $input = "";
             // $input = " ";
             // $input = "r@nd0m_nVmb3r$!#";
-            // $input = "15";
             // $input = "1 5";
             // $input = "&* !!";
             // $input = "&*84 &@ 33!!";
             // $input = "15*$%%89";
+            $input = "15";
 
             //Act
-            $result = $test_PingPongGenerator->combineCount($input);
+            $result = $test_PingPongGenerator->count($input);
             var_dump($result);
 
             //Assert
